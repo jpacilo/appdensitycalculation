b8905bf7bf4d4341a9bdc2870d06093151cffeae
J. Acilo <joshuaacilo.13@gmail.com>
2019-03-20 08:16:52 +0800

    Create CityDensity.R

---
+++ CityDensity.R
@@ -0,0 +1,4 @@
+# Using the population.csv and regionarea.csv files, calculate the population density of all Cities in the Philippines.
+# However, since only areas for regions are provided (in square kilometers), assume that all cities in a particular region will have equal land area. (E.g. Area of City in Region 1 = Area of Region 1/Number of Cities in Region 1)
+# The R Code should be able to output a CSV file that lists down the top 5 cities with the highest densities.
+
