f81b095dee7ce30fbb0f0a44b21756314bd197a0
J. Acilo <joshuaacilo.13@gmail.com>
2019-03-20 08:16:13 +0800

    Create BarangayDensity.R

---
+++ BarangayDensity.R
@@ -0,0 +1,3 @@
+# Using the population.csv and regionarea.csv files, calculate the population density (persons per square km) of all barangays in the Philippines.
+# However, since only areas for regions are provided (in square kilometers), assume that the barangays in a particular region will have equal land area. (E.g. Area of Barangay in Region 1 = Area of Region 1/Number of Barangays in Region 1)
+# The R Code should be able to output a CSV file that lists down the top 5 barangays with the highest densities.
